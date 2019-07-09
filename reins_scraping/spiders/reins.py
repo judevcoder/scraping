309fba5580989600b78b2dc88a35246e1ff791aa
softdevstorm <softdevstorm@gmail.com>
2019-07-09 05:41:30 -0400

    updated

--- reins_scraping/spiders/reins.py
+++ reins_scraping/spiders/reins.py
@@ -69,7 +69,7 @@ class RenisSpider(scrapy.Spider):
         if captcha_answer:
             print("captcha solved")
         else:
-            print("Ca[tcha didn't solve")
+            print("Captcha can't be solved, run spider again...")
             return
 
         self.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,ima'
@@ -120,18 +120,197 @@ class RenisSpider(scrapy.Spider):
                 url=sale_url,
                 method='POST',
                 formdata=form_data,
-                callback=self.parse_list_page,
+                callback=self.input_process,
                 headers=self.headers,
                 dont_filter=True
             )
+
+    def input_process(self, response):
+        action = response.xpath('//form[@name="Bkkn001Form"]/@action').extract()[0]
+        req_url = urljoin(response.url, action)
+        token = response.xpath('//input[@name="org.apache.struts.taglib.html.TOKEN"]/@value').extract()[0]
+
+        self.headers['Referer'] = response.url
+
+        random_id = response.xpath('//input[@id="randomID"]/@value').extract()[0]
+        seni_gen_gameen_id = response.xpath('//input[@name="seniGenGamenID"]/@value').extract()[0]
+
+        form_data = {
+            'org.apache.struts.taglib.html.TOKEN': token,
+            'randomID': random_id,
+            'contextPath': '/reins',
+            'event': 'forward_searchbabi',
+            'bbTtKbn': '1',
+            'stateMode': '',
+            'stWttBg': '',
+            'hzMi': '',
+            'zkSyKbn': '2',
+            'stJyk': '',
+            'bkknShmk1': '',
+            'bkknShmk2': '',
+            'bkknShbt1': '03',
+            'bkknShmkDispList1': '',
+            'bkknShmkDispList1': '',
+            'bkknShmkDispList1': '',
+            'bkknShmkDispList1': '',
+            'bkknShmkDispList1': '',
+            'bkknShmkDispList1': '',
+            'bkknShbt2': '',
+            'bkknShmkDispList2': '',
+            'bkknShmkDispList2': '',
+            'bkknShmkDispList2': '',
+            'bkknShmkDispList2': '',
+            'bkknShmkDispList2': '',
+            'shtkChkKbnShti': '0',
+            'shkcknShriSti': '0',
+            'shgUmKbn': '1',
+            'trhkJyukyu': '0',
+            'tdfkMi1': '東京都',
+            'shzicmi1_1': '',
+            'shzicmi2_1': '',
+            'shzicJyk_1': '1',
+            'ttmnmi_1': '',
+            'ttmnJyk_1': '1',
+            'tdfkMi2': '',
+            'shzicmi1_2': '',
+            'shzicmi2_2': '',
+            'shzicJyk_2': '1',
+            'ttmnmi_2': '',
+            'ttmnJyk_2': '1',
+            'tdfkMi3': '',
+            'shzicmi1_3': '',
+            'shzicmi2_3': '',
+            'shzicJyk_3': '1',
+            'ttmnmi_3': '',
+            'ttmnJyk_3': '1',
+            'ensnmi1': '',
+            'ekmiFrom1': '',
+            'ekmiTo1': '',
+            'thNyrkt1': '',
+            'thMbKbn1': '',
+            'krmKm1': '',
+            'bsB1': '',
+            'ensnmi2': '',
+            'ekmiFrom2': '',
+            'ekmiTo2': '',
+            'thNyrkt2': '',
+            'thMbKbn2': '',
+            'krmKm2': '',
+            'bsB2': '',
+            'ensnmi3': '',
+            'ekmiFrom3': '',
+            'ekmiTo3': '',
+            'thNyrkt3': '',
+            'thMbKbn3': '',
+            'krmKm3': '',
+            'bsB3': '',
+            'bsRsmi': '',
+            'bsTmiSh': '',
+            'tihNyrkt': '',
+            'tihMbKbn': '',
+            'sotKtu': '',
+            'sotKtuNyrkt': '',
+            'sotKtuMbKbn': '',
+            'kkkuCnryuFrom': '',
+            'kkkuCnryuTo': '',
+            'siykKkkuCnryuFrom': '',
+            'siykKkkuCnryuTo': '',
+            'tbTnkFrom': '',
+            'tbTnkTo': '',
+            'siykTbTnkFrom': '',
+            'siykTbTnkTo': '',
+            'tcMnskFrom': '',
+            'tcMnskTo': '',
+            'ttmnMnskFrom': '',
+            'ttmnMnskTo': '',
+            'snyuMnskFrom': '',
+            'snyuMnskTo': '',
+            'mdrHysuFrom': '',
+            'mdrHysuTo': '',
+            'shzikiFrom': '',
+            'shzikiTo': '',
+            'blcnyHuku': '',
+            'stdoHuku': '',
+            'stdoJyukyu': '',
+            'stdoStmn': '',
+            'stdoFkin': '',
+            'tskikk': '',
+            'yutCik': '',
+            'sitkYut': '',
+            'ktcJok': '',
+            'chushjyuZih': '',
+            'cknngtYearFrom': '',
+            'cknngtMonthFrom': '',
+            'cknngtYearTo': '',
+            'cknngtMonthTo': '',
+            'kjkrngGgFrom': 'R',
+            'kjkrngYearFrom': '',
+            'kjkrngMonthFrom': '',
+            'kjkrngGgTo': 'R',
+            'kjkrngYearTo': '',
+            'kjkrngMonthTo': '',
+            'optId': '',
+            'strStbJok': '',
+            'bk1': '',
+            'shuhnKnkyu': '',
+            'turkKknFlg': '1',
+            'turkNngppGgFrom': 'R',
+            'turkNngppNenFrom': '',
+            'turkNngppGatuFrom': '',
+            'turkNngppHiFrom': '',
+            'turkNngppGgTo': 'R',
+            'turkNngppNenTo': '',
+            'turkNngppGatuTo': '',
+            'turkNngppHiTo': '',
+            'hcKknFlg': '1',
+            'hnkuNngppGgFrom': 'R',
+            'hnkuNngppNenFrom': '',
+            'hnkuNngppGatuFrom': '',
+            'hnkuNngppHiFrom': '',
+            'hnkuNngppGgTo': 'R',
+            'hnkuNngppNenTo': '',
+            'hnkuNngppGatuTo': '',
+            'hnkuNngppHiTo': '',
+            'siykKknFlg': '5',
+            'siykNngppGgFrom': 'R',
+            'siykNngppNenFrom': '1',
+            'siykNngppGatuFrom': '7',
+            'siykNngppHiFrom': '1',
+            'siykNngppGgTo': 'R',
+            'siykNngppNenTo': '1',
+            'siykNngppGatuTo': '7',
+            'siykNngppHiTo': '2',
+            'siykTurkKknFlg': '1',
+            'siykTurkNngppGgFrom': 'R',
+            'siykTurkNngppNenFrom': '',
+            'siykTurkNngppGatuFrom': '',
+            'siykTurkNngppHiFrom': '',
+            'siykTurkNngppGgTo': 'R',
+            'siykTurkNngppNenTo': '',
+            'siykTurkNngppGatuTo': '',
+            'siykTurkNngppHiTo': '',
+            'seniMotFlg': '',
+            'seniGenGamenID': seni_gen_gameen_id
+        }
+
+        return FormRequest(
+            url=req_url,
+            method='POST',
+            formdata=form_data,
+            callback=self.parse_list_page,
+            headers=self.headers,
+            dont_filter=True
+        )
 
     def parse_list_page(self, response):
         res = response
 
     def solve_captch_process(self, session_id):
 
-        if self.trying_captcha_solve_count >= 10:
+        if self.trying_captcha_solve_count >= 20:
             return
+
+        answer = None
 
         try:
             self.headers['Accept'] = 'image/webp,image/apng,image/*,*/*;q=0.8'
@@ -155,13 +334,15 @@ class RenisSpider(scrapy.Spider):
             answer = s.get(self.captcha_res_url + '?key={}&action=get&id={}'.format(self.api_key, captcha_id)).text.split('|')[1]
             time.sleep(10)
 
-            return answer
-
         except Exception as e:
             print(e)
             print("captcha can't be solved, retrying now...")
             self.trying_captcha_solve_count += 1
             self.solve_captch_process(session_id)
+
+        if answer:
+            print(answer)
+            return answer
 
     @staticmethod
     def _clean_text(text):
